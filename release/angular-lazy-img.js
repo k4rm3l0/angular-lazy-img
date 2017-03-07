40754842c66e6f0330114e037fcd487f76c87d12
Carmelo Orlando <carmelo.orlando@etuitus.it>
2017-03-07 13:25:33 +0100

    HQ Images

--- release/angular-lazy-img.js
+++ release/angular-lazy-img.js
@@ -218,25 +218,48 @@ angular.module('angularLazyImg').factory('lazyImgHelpers', [
 ]);
 angular.module('angularLazyImg')
   .directive('lazyImg', [
-    '$rootScope', 'LazyImgMagic', function ($rootScope, LazyImgMagic) {
+    '$rootScope', 'LazyImgMagic', '$timeout', function ($rootScope, LazyImgMagic, $timeout) {
       'use strict';
 
+      var _THRESHOLD_VALUE = 120;
+
       function link(scope, element, attributes) {
-        var lazyImage = new LazyImgMagic(element),
-            deregister = attributes.$observe('lazyImg', function (newSource) {
+        var lazyImage = new LazyImgMagic(element);
+        var deregister = attributes.$observe('lazyImg', function (newSource) {
           if (newSource) {
             deregister();
             lazyImage.setSource(newSource);
           }
         });
+        var deregister2 = attributes.$observe('lazyImgZoom', function(value) {
+              var zoom = parseFloat(value);
+              if(attributes.lazyImg && attributes.lazyImg.length>0 && zoom){
+                  if(zoom > _THRESHOLD_VALUE){
+                      deregister2();
+                      console.log('lazyImgZoom', attributes.lazyImgHq);
+                      if(attributes.lazyImgHq){
+                          lazyImage.setSource(attributes.lazyImgHq);
+                      }
+                  }
+              }
+          });
         scope.$on('$destroy', function () {
           lazyImage.removeImage();
         });
         $rootScope.$on('lazyImg.runCheck', function () {
           lazyImage.checkImages();
+
         });
         $rootScope.$on('lazyImg:refresh', function () {
           lazyImage.checkImages();
+            console.log('CIAONE');
+        });
+        $rootScope.$on('lazyImg:edit', function (event, data) {
+            lazyImage.removeImage();
+            lazyImage.setSource(data);
+            $timeout(function () {
+                lazyImage.checkImages();
+            });
         });
       }
 
