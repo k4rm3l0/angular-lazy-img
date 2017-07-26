ef9af6602562fc1b0a7f019dd898dff3c54e2209
Carmelo Orlando <carmelo.orlando@etuitus.it>
2017-07-26 16:23:50 +0200

    added scroll orientation

--- src/angular-lazy-img-directive.js
+++ src/angular-lazy-img-directive.js
@@ -1,63 +1,74 @@
 angular.module('angularLazyImg')
-  .directive('lazyImg', [
-    '$rootScope', 'LazyImgMagic', '$timeout', function ($rootScope, LazyImgMagic, $timeout) {
-      'use strict';
+    .directive('lazyImg', [
+        '$rootScope', 'LazyImgMagic', function ($rootScope, LazyImgMagic) {
+            'use strict';
 
-      var _THRESHOLD_HQ     = 120,
-          _THRESHOLD_XHQ    = 160;
+            var _THRESHOLD_HQ = 120,
+                _THRESHOLD_XHQ = 160;
 
-      function link(scope, element, attributes) {
-        var lazyImage = new LazyImgMagic(element);
-        var deregister = attributes.$observe('lazyImg', function (newSource) {
-          if (newSource) {
-            deregister();
-            lazyImage.setSource(newSource);
-          }
-        });
-        var deregisterObserver = attributes.$observe('lazyImgZoom', function(newZoom) {
+            function link(scope, element, attributes) {
 
-            var zoom = parseFloat(newZoom) || 100;
+                var lazyImage = new LazyImgMagic(element);
 
-            if(attributes.lazyImgXhq && zoom > _THRESHOLD_XHQ){
-                lazyImage.setSource(attributes.lazyImgXhq);
-                lazyImage.checkImages();
-            }else if(attributes.lazyImgHq && zoom > _THRESHOLD_HQ){
-                lazyImage.setSource(attributes.lazyImgHq);
-                lazyImage.checkImages();
-            }else{
-                lazyImage.setSource(attributes.lazyImg);
-                lazyImage.checkImages();
+                var deregister = attributes.$observe('lazyImg', function (newSource) {
+                    if (newSource) {
+                        deregister();
+                        lazyImage.setSource(newSource);
+                    }
+                });
+
+                attributes.$observe('lazyImgZoom', function (newZoom) {
+                    var zoom = parseFloat(newZoom) || 100;
+                    if (attributes.lazyImgXhq && zoom > _THRESHOLD_XHQ) {
+                        lazyImage.setSource(attributes.lazyImgXhq);
+                    } else if (attributes.lazyImgHq && zoom > _THRESHOLD_HQ) {
+                        lazyImage.setSource(attributes.lazyImgHq);
+                    } else {
+                        lazyImage.setSource(attributes.lazyImg);
+                    }
+                    lazyImage.checkImages();
+                });
+
+                scope.$on('$destroy', function () {
+                    lazyImage.removeImage();
+                });
+
+                $rootScope.$on('lazyImg.runCheck', function () {
+                    lazyImage.checkImages();
+
+                });
+
+                $rootScope.$on('lazyImg:refresh', function () {
+                    lazyImage.checkImages();
+                });
             }
-        });
-        scope.$on('$destroy', function () {
-          lazyImage.removeImage();
-        });
-        $rootScope.$on('lazyImg.runCheck', function () {
-          lazyImage.checkImages();
 
-        });
-        $rootScope.$on('lazyImg:refresh', function () {
-          lazyImage.checkImages();
-        });
-      }
-      return {
-        link: link,
-        restrict: 'A'
-      };
-    }
-  ])
-  .directive('lazyImgContainer', [
-    'LazyImgMagic', function (LazyImgMagic) {
-      'use strict';
-      function link(scope, element) {
-        LazyImgMagic.addContainer(element);
-        scope.$on('$destroy', function () {
-          LazyImgMagic.removeContainer(element);
-        });
-      }
-      return {
-        link: link,
-        restrict: 'A'
-      };
-    }
-  ]);
+            return {
+                link: link,
+                restrict: 'A'
+            };
+        }
+    ])
+    .directive('lazyImgContainer', [
+        'LazyImgMagic', function (LazyImgMagic) {
+            'use strict';
+
+            function link(scope, element, attributes) {
+                LazyImgMagic.addContainer(element);
+                scope.$on('$destroy', function () {
+                    LazyImgMagic.removeContainer(element);
+                });
+                var deregister = attributes.$observe('scrollOrientation', function (orientation) {
+                    if (orientation) {
+                        deregister();
+                        console.log(orientation);
+                    }
+                });
+            }
+
+            return {
+                link: link,
+                restrict: 'A'
+            };
+        }
+    ]);
